d25c1380d1eebebe8703cb6e44ec2f0b575081ed
Aravind Kumar <ara3194@gmail.com>
2021-04-27 16:34:40 +0530

    Edited base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,5 @@
-const baseUrl ="http://localhost:3000";
+const baseUrl = process.env.NODE_ENV !== "production" 
+? "http://localhost:3000" 
+: "https://tweezak-aravind.herokuapp.com";
 
 export default baseUrl;
